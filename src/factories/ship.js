3dabdbe26326888bde748f836b5aabf295cd56f2
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-07-17 14:41:40 +0200

    Feat: Implement Ship() factory function

---
+++ src/factories/ship.js
@@ -0,0 +1,21 @@
+const Ship = (length) => {
+  if (!length) {
+    return false;
+  }
+  return {
+    length,
+    timesHit: 0,
+    sunk: false,
+    hit() {
+      this.timesHit += 1;
+      this.isSunk();
+    },
+    isSunk() {
+      if (this.timesHit >= this.length) {
+        this.sunk = true;
+      }
+    },
+  };
+};
+
+export default Ship;
---
+++ src/factories/tests/ship.test.js
@@ -0,0 +1,21 @@
+import Ship from '../ship';
+
+describe('Ship', () => {
+  test('should increase the number of hits', () => {
+    const ship = Ship(4);
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+
+    ship.hit();
+    ship.hit();
+    expect(ship.timesHit).toBe(3);
+  });
+  test('should know when its sunk', () => {
+    const ship = Ship(2);
+    ship.hit();
+    expect(ship.sunk).toBe(false);
+
+    ship.hit();
+    expect(ship.sunk).toBe(true);
+  });
+});
