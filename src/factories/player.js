95d7d9416b11c0019cce9b9c3b49368bc6cfc5be
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-07-20 16:23:39 +0200

    Feat: Enable computer player to perform random attacks

--- src/factories/player.js
+++ src/factories/player.js
@@ -2,23 +2,48 @@ import GameBoard from './game-board';
 
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-const Player = (isHuman, name = 'Player') => {
-  const gameBoard = GameBoard(10);
+const Player = (isHuman, boardSize = 10) => {
+  const gameBoard = GameBoard(boardSize);
+  const possibleTargets = [];
+
+  let currentRow = 1;
+  let currentColumn = 1;
+  for (let i = 0; i < boardSize * boardSize; i += 1) {
+    possibleTargets.push([currentRow, currentColumn]);
+    if (currentColumn < boardSize) {
+      currentColumn += 1;
+    } else {
+      currentColumn = 1;
+      currentRow += 1;
+    }
+  }
 
   return {
     isHuman,
-    name,
     gameBoard,
+    boardSize,
+    possibleTargets,
     targetedCoordinates: [],
 
-    chooseRandomAttack(possibleTargets) {
-      if (possibleTargets.length <= 0) {
+    chooseRandomAttack(targetList = this.possibleTargets) {
+      if (targetList.length <= 0) {
         return false;
       }
-      const index = getRandomInt(possibleTargets.length);
-      const attackCoordinates = possibleTargets[index];
+      const index = getRandomInt(targetList.length);
+      const attackCoordinates = targetList[index];
 
       return attackCoordinates;
+    },
+
+    performRandomAttack(enemyPlayer) {
+      const randomAttack = this.chooseRandomAttack();
+      this.targetedCoordinates.push(randomAttack);
+      const index = this.possibleTargets.findIndex(
+        (value) => value[0] === randomAttack[0] && value[1] === randomAttack[1]
+      );
+      this.possibleTargets.splice(index, 1);
+
+      enemyPlayer.gameBoard.receiveHit(randomAttack);
     },
   };
 };
--- src/factories/tests/player.test.js
+++ src/factories/tests/player.test.js
@@ -12,4 +12,19 @@ describe('Computer-player', () => {
     const chosenMove = player.chooseRandomAttack(moves);
     expect(moves).toContainEqual(chosenMove);
   });
+
+  test('should modify possible moves after random attacks', () => {
+    const player = Player(false);
+    const enemyPlayer = Player(true);
+    player.performRandomAttack(enemyPlayer);
+
+    expect(player.targetedCoordinates.length).toBe(1);
+    const chosenMove = player.targetedCoordinates[0];
+
+    expect(
+      player.possibleTargets.some((target) => {
+        return target[0] === chosenMove[0] && target[1] === chosenMove[1];
+      })
+    ).toBeFalsy();
+  });
 });
