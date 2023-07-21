e4d10e5947d066fa17d8ea2b3ad42ef9873ecf56
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-07-21 19:13:12 +0200

    Feat: Implement renderGameBoards()

---
+++ src/dom.js
@@ -0,0 +1,49 @@
+const gameBoardsElement = document.querySelector('.gameboards');
+
+const createDomElement = (
+  elementType = 'div',
+  classesString = '',
+  innerHTML = ''
+) => {
+  const element = document.createElement(elementType);
+  element.className = classesString;
+  element.innerHTML = innerHTML;
+
+  return element;
+};
+
+const renderGameBoards = (playerBoard, computerBoard, gridSize = 10) => {
+  gameBoardsElement.innerHTML = '';
+  const playerTiles = playerBoard.tiles;
+  const computerTiles = computerBoard.tiles;
+
+  const playerGameBoardElement = createDomElement(
+    'div',
+    'gameboard gameboard-player'
+  );
+  playerTiles.forEach((tile) => {
+    const tileElement = createDomElement('div', 'tile');
+    tileElement.setAttribute('data-row', tile.row);
+    tileElement.setAttribute('data-column', tile.column);
+    playerGameBoardElement.appendChild(tileElement);
+  });
+
+  const computerGameBoardElement = createDomElement(
+    'div',
+    'gameboard gameboard-computer'
+  );
+  computerTiles.forEach((tile) => {
+    const tileElement = createDomElement('div', 'tile');
+    tileElement.setAttribute('data-row', tile.row);
+    tileElement.setAttribute('data-column', tile.column);
+    computerGameBoardElement.appendChild(tileElement);
+  });
+
+  playerGameBoardElement.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  computerGameBoardElement.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+  gameBoardsElement.appendChild(playerGameBoardElement);
+  gameBoardsElement.appendChild(computerGameBoardElement);
+};
+
+export default renderGameBoards;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './main.css';
 import './img/background.jpg';
 import GameBoard from './factories/game-board';
+import renderGameBoards from './dom';
 
-const gameBoard = GameBoard(10);
-gameBoard.placeShip([2, 11]);
+const BOARD_SIZE = 10;
+
+const gameBoard = GameBoard(BOARD_SIZE);
+const otherGameBoard = GameBoard(BOARD_SIZE);
+
+renderGameBoards(gameBoard, otherGameBoard, BOARD_SIZE);
